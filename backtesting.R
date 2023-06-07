2df9dea8e351a444c0d5e6119fdc8459aeaf416c
NicHrg <nico.herrig@gmx.de>
2023-06-07 15:11:17 +0200

    Update backtesting.R

--- backtesting.R
+++ backtesting.R
@@ -1,13 +1,24 @@
 ######################### BENCHMARKING #########################################
 
-
-install.packages("quarks")
 library(quarks) # alternative package for backtesting
+
+############################# PRE-LOADING ######################################
+# setting working directory
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+print( getwd() )
+
+
+# sourcing initialisation parameters
+source("traditional_models.R")
+source("GARCH_models.R")
+
+
+
 
 # Christoffersen test 
 ChristoffersenBacktestForUnconditionalCoverage()
 ChristoffersenBacktestForIndependence()
-
 
 
 # combined tests (from quarks)
