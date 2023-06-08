4c72413377334d6b04ab145859322524d3668470
NicHrg <nico.herrig@gmx.de>
2023-06-08 16:37:13 +0200

    Update utility.R

--- utility.R
+++ utility.R
@@ -7,6 +7,7 @@ library(lubridate)
 library(quantmod)
 library(fGarch)
 library(Dowd)
+library(readxl)
 
 ############################# PRE-LOADING ######################################
 # setting working directory
